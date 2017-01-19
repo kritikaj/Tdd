5e47c3b622bcec6203ecd48b46892fc3ce0002cb
KRITIKA JAJODIA <kritikaj@thoughtworks.com>
2017-01-19 17:33:41 +0530

    [Kritika] | Refactored divide() in Calculator

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -12,18 +12,9 @@ public class Calculator {
   }
 
   public int divide(int dividend, int divisor) throws UndefinedOperation {
-    if(divisor>=0 ){
-      if(dividend > 0){
-        if((divisor == 0) || (divisor>dividend)){
-          throw new UndefinedOperation();
-        }
-      }
-      else if((dividend < 0) && (abs(dividend)<divisor)){
-        throw new UndefinedOperation();
-      }
-
+    if((divisor == 0) || (abs(divisor)>abs(dividend))){
+      throw new UndefinedOperation();
     }
-    float result = (float) dividend/divisor;
-    return (int) floor(result);
+    return (int) floor( (float) dividend/divisor);
   }
 }
